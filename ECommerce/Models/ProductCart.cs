431bafc7e4094304403d7081cea5fdc9ea4daad1
ahmed-nady17 <ahmedsalah@h-eng.helwan.edu.eg>
2022-03-04 03:14:32 +0200

    update

--- ECommerce/Models/ProductCart.cs
+++ ECommerce/Models/ProductCart.cs
@@ -1,6 +1,19 @@
-﻿namespace Ecommerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Models
 {
     public class ProductCart
     {
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual Cart Cart { get; set; }
+
     }
 }
