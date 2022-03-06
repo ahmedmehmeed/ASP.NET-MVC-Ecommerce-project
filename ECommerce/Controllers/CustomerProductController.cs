91dec6cbb1dd2d72fc66acb7cf5b405ab6c8edb6
EngyussienMoawed <Engyhussienmoawed@gmail.com>
2022-03-07 01:27:42 +0200

    Views

---
+++ ECommerce/Controllers/CustomerProductController.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Ecommerce.Controllers
+{
+    public class CustomerProductController : Controller
+    {
+
+        IProductRepository productRepository;
+        public CustomerProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = productRepository.Getall();
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = productRepository.GetById(id);
+            return View(product);
+        }
+    }
+}
