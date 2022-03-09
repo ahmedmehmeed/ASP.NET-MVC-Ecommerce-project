78489ccef2557af75979156cad8110a4cddf4b18
hendahmed98 <100911641+hendahmed98@users.noreply.github.com>
2022-03-09 14:13:20 +0200

    views

--- ECommerce/Controllers/CustomerProductController.cs
+++ ECommerce/Controllers/CustomerProductController.cs
@@ -2,22 +2,33 @@
 using Ecommerce.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ecommerce.Controllers
 {
     public class CustomerProductController : Controller
     {
-
+        ECommerceEntity context;
+        ICategoryRepository categoryRepository;
         IProductRepository productRepository;
-        public CustomerProductController(IProductRepository productRepository)
+        public CustomerProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
             this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
+            context=new ECommerceEntity();
         }
         public IActionResult Index()
         {
-            List<Product> products = productRepository.Getall();
-            return View(products);
+            List<Category> categories = categoryRepository.Getall();
+            return View(categories);
         }
+
+        public IActionResult Products(int CatId)
+        {
+            List<Product> products = context.Products.Where(n => n.CategoryId == CatId).ToList();
+            return PartialView("_Products", products);
+        }
+
 
         public IActionResult Details(int id)
         {
